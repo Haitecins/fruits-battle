ef2c9f5e39bc83d587f10f1f5a5238895fc09f9e
chiyukikana <chiyukikana@gmail.com>
2022-01-11 12:13:56 +0800

    UPDATE 添加浏览器兼容性

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from "vite";
+import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
+  plugins: [
+    legacy({
+      targets: ["defaults", "not IE 11"],
+    }),
+  ],
   server: {
     port: 8080,
   },
   build: {
+    manifest: true,
     outDir: "out",
-    assetsInlineLimit: 32768,
+    assetsDir: "static",
+    sourcemap: true,
   },
 });
