6e0cdf582320845b7524e6d3c41fededc7c34924
Chiyuki Kana <chiyukikana@gmail.com>
2022-04-17 19:09:23 +0800

    perf(event): 修改名称event为ev

--- src/modules/utils/ios.ts
+++ src/modules/utils/ios.ts
@@ -1,17 +1,17 @@
 // 禁止Safari浏览器的弹性滚动
 let lastTouchEnd = 0;
 
-document.addEventListener("touchstart", (event) => {
-  if (event.touches.length > 1) event.preventDefault();
+document.addEventListener("touchstart", (ev) => {
+  if (ev.touches.length > 1) ev.preventDefault();
 });
 document.addEventListener(
   "touchend",
-  (event) => {
+  (ev) => {
     const now = new Date().getTime();
-    if (now - lastTouchEnd <= 300) event.preventDefault();
+    if (now - lastTouchEnd <= 300) ev.preventDefault();
     lastTouchEnd = now;
   },
   false
 );
-document.addEventListener("gesturestart", (event) => event.preventDefault());
-document.addEventListener("dblclick", (event) => event.preventDefault());
+document.addEventListener("gesturestart", (ev) => ev.preventDefault());
+document.addEventListener("dblclick", (ev) => ev.preventDefault());
